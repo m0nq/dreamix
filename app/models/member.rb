fc485e944817277a2be4a16d7fe7f3df6e950124
m0nq <monq.wellington@gmail.com>
2016-02-22 11:19:24 -0800

    rebuild models.

--- app/models/member.rb
+++
@@ -1,8 +0,0 @@
-class Member < ActiveRecord::Base
-  has_many :songs, :through => :stream
-  has_many :favorites
-  has_many :shares
-
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :email, presence: true, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, message: "Please enter a valid email" }
-end
